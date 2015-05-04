8572fce1775a177610ef9d5f9082557a8f19191a
NullVoxPopuli <LPSego3+dev@gmail.com>
2015-05-04 01:05:39 -0400

    increased coverage of the assume control feature

--- spec/features/assume_control_spec.rb
+++ spec/features/assume_control_spec.rb
@@ -3,33 +3,44 @@ require 'spec_helper'
 describe "AssumeControl" do
 
     describe '#assume_control' do
-
-      it 'does not find the cache token' do
-        user = create(:user)
-        user.confirmed_at = Time.now
-        user.save
+      before(:each) do
+        user = create(:user, confirmed_at: Time.now, first_name: "original")
         login_as(user, scope: :user)
+      end
 
-        visit '/users/-1/assume_control'
-
-        expect(page).to have_content("User not found or token expired")
+      after(:each) do
         expect(page.current_url).to end_with('/')
       end
 
-      it 'cannot find the user (maybe due to deletion)' do
+      it 'does not find the cache token' do
+        visit '/users/-1/assume_control'
 
+        expect(page).to have_content("User not found or token expired")
+      end
+
+      it 'cannot find the user (maybe due to deletion)' do
+        Cache.set('1', -1)
+        visit '/users/1/assume_control'
+
+        expect(page).to have_content("User not found.")
       end
 
       it 'signs the new user in' do
+        new_user = create(:user, confirmed_at: Time.now, first_name: "new_user")
+        Cache.set('1', new_user.id)
 
+        visit '/users/1/assume_control'
+
+        expect(page).to have_content("new_user")
       end
 
       it 'tells us we are impersonating the user' do
+        new_user = create(:user, confirmed_at: Time.now, first_name: "new_user")
+        Cache.set('1', new_user.id)
 
-      end
+        visit '/users/1/assume_control'
 
-      it 'redirects to the root url upon success' do
-
+        expect(page).to have_content("You are now #{new_user.name}.")
       end
 
     end
