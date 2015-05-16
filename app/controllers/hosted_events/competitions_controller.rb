53f4fe5859ba9a5c9210f05d85da2b09b472d389
NullVoxPopuli <LPSego3+dev@gmail.com>
2015-05-15 23:49:36 -0400

    minor code cleanup

--- app/controllers/hosted_events/competitions_controller.rb
+++ app/controllers/hosted_events/competitions_controller.rb
@@ -1,6 +1,12 @@
 class HostedEvents::CompetitionsController < ApplicationController
-  before_action :set_event
-  before_action :set_competition, only: [:show, :edit, :update, :destroy]
+  include SetsEvent
+  include LazyCrud
+
+  set_resource Competition
+  set_resource_parent Event
+  set_param_whitelist(
+    :name, :initial_price, :at_the_door_price, :kind
+  )
 
   layout "edit_event"
 
@@ -25,70 +31,4 @@ class HostedEvents::CompetitionsController < ApplicationController
       end
   end
 
-  # GET /competitions/new
-  def new
-    @competition = Competition.new
-  end
-
-  # GET /competitions/1/edit
-  def edit
-  end
-
-  # POST /competitions
-  # POST /competitions.json
-  def create
-    @competition = Competition.new(competition_params)
-
-    @competition.event = @event
-    respond_to do |format|
-      if @competition.save
-        format.html { redirect_to hosted_event_competitions_path(@event.id), notice: 'Competition was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @competition }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @competition.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /competitions/1
-  # PATCH/PUT /competitions/1.json
-  def update
-    @competition.event = @event
-
-    respond_to do |format|
-      if @competition.update(competition_params)
-        format.html { redirect_to hosted_event_competitions_path(@event.id), notice: 'Competition was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @competition.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /competitions/1
-  # DELETE /competitions/1.json
-  def destroy
-    @competition.destroy
-    respond_to do |format|
-      format.html {
-        flash[:notice] = "#{@competition.name} has been deleted."
-        redirect_to hosted_event_competitions_path(@event)
-      }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-  # Use callbacks to share common setup or constraints between actions.
-
-  def set_competition
-    @competition = @event.competitions.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def competition_params
-    params[:competition].permit(:name, :initial_price, :at_the_door_price, :kind)
-  end
 end
