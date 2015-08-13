c67b1f6fe6a8ec56efc433f2e3c3778df285cded
NullVoxPopuli <LPSego3+dev@gmail.com>
2015-08-12 22:30:06 -0400

    ember error reporting works now

--- app/controllers/api/errors_controller.rb
+++ app/controllers/api/errors_controller.rb
@@ -1,16 +1,38 @@
 class Api::ErrorsController < APIController
 
   def from_ember
-    # binding.pry
-    error = Exceptions::FrontEndError.new
+    error = Exceptions::FrontEndError.new(
+      message: error_params[:message],
+      backtrace: backtrace_as_array
+    )
+
     data = {
-      class: '',
-      method: '',
-      message: ''
+      class: error_params[:class],
+      method: error_params[:method],
+      message: error_params[:message],
+      params: error_params[:params]
     }
+
     PartyFoul::RacklessExceptionHandler.handle(
       error,
       data
+    )
+
+    render json:  { error: error_params.to_json }
+  end
+
+  private
+
+  def backtrace_as_array
+    error_params[:stack].try(:split, "\n") || []
+  end
+
+  def error_params
+    params[:error].permit(
+      :message, :stack,
+      :class, :method,
+      :cause,
+      :params
     )
   end
 
