424b6408c537b1c0e7343777cc19c725f197a33e
NullVoxPopuli <LPSego3+dev@gmail.com>
2015-08-19 19:59:08 -0400

    add url to orders

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -5,11 +5,15 @@ class OrderSerializer < ActiveModel::Serializer
     :host_id, :host_type,
     :paid_amount, :net_amount_received, :total_fee_amount,
     :paid, :payment_method,
-    :host_name,
+    :host_name, :host_url
     :created_at
 
     def host_name
       object.host.name
+    end
+
+    def host_url
+      object.host.url
     end
 
 
