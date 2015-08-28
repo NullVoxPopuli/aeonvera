bcf13a9ba0108bf845281ffd763a34aec8f4da17
NullVoxPopuli <LPSego3+dev@gmail.com>
2015-08-28 19:05:10 -0400

    oops, need teh key specified

--- app/controllers/api/upcoming_events_controller.rb
+++ app/controllers/api/upcoming_events_controller.rb
@@ -2,7 +2,7 @@ class Api::UpcomingEventsController < APIController
 
   def index
     @events = Event.upcoming
-    render json: @events, each_serializer: UpcomingEventSerializer
+    render json: @events, each_serializer: UpcomingEventSerializer, root: :upcoming_event
   end
 
 end
