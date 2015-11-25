66e8e860a9b43923267aea1517aaa4e15f75d1c0
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-11-25 15:34:03 -0500

    add package policy spec to update travis

--- spec/policies/package_policy_spec.rb
+++ spec/policies/package_policy_spec.rb
@@ -5,7 +5,11 @@ describe PackagePolicy do
   context 'can be read?' do
 
     it 'by the event owner' do
+      package = create(:package)
+      policy = PackagePolicy.new(package.event.hosted_by, package)
+      result = policy.read?
 
+      expect(result).to eq true
     end
 
     it 'by a registrant' do
