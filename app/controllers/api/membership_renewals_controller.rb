65b433ac98a127bf7e79e2541b9780b99eafa17e
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-02-25 06:45:40 -0500

    added params

--- app/controllers/api/membership_renewals_controller.rb
+++ app/controllers/api/membership_renewals_controller.rb
@@ -6,4 +6,29 @@ class Api::MembershipRenewalsController < Api::ResourceController
     render json: model, include: params[:include], each_serializer: MembershipRenewalSerializer
   end
 
+  private
+
+  def create_membership_renewal_params
+    data = params.require(:data)
+    attributes = data
+      .require(:attributes)
+      .permit(:start_date)
+
+    relationships = data.require(:relationships)
+
+    user_id = relationships
+      .require(:member)
+      .require(:data)
+      .permit(:id)
+
+    membership_option_id = relationships
+      .require(:membership_option)
+      .require(:data)
+      .permit(:id)
+
+    attributes.merge(
+      user_id: user_id,
+      membership_option_id: membership_option_id)
+  end
+
 end
