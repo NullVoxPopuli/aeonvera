28e9c23a61497e77a303df17e4b4793535e72864
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-03-17 11:46:37 -0400

    fix integration serializing

--- app/serializers/integration_serializer.rb
+++ app/serializers/integration_serializer.rb
@@ -2,18 +2,14 @@
 class IntegrationSerializer < ActiveModel::Serializer
 
   attributes :id,
-    :name, :owner_id, :owner_type,
+    :kind, :owner_id, :owner_type,
     :publishable_key
 
   belongs_to :owner
 
-  def name
-    object.kind
-  end
-
   def publishable_key
     if object.kind == Integration::STRIPE
-      return object.config[:stripe_publishable_key]
+      return object.config['stripe_publishable_key']
     end
   end
 
