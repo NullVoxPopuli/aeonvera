f44ebb742548b6910e0c5a4f0c6366f3a896d569
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-03-28 22:58:07 -0400

    maybe finished the comp migration?

--- db/migrate/20160328115508_migrate_competition_responses_to_order_line_items.rb
+++ db/migrate/20160328115508_migrate_competition_responses_to_order_line_items.rb
@@ -1,7 +1,26 @@
 class MigrateCompetitionResponsesToOrderLineItems < ActiveRecord::Migration
   def up
-    Attendance.all.each do |a|
+    CompetitionResponse.all.each do |cr|
+      a = Attendance.with_deleted.find_by_id(cr.attendance_id)
+      next unless a
 
+      partner_name = cr.partner_name
+      dance_orientation = cr.dance_orientation
+
+      a.orders.each do |order|
+        order.line_items.each do |order_line_item|
+          id = order_line_item.line_item_id
+          kind = order_line_item.line_item_type
+          next unless kind == Competition.name
+          next unless id == cr.competition_id
+
+          order_line_item.partner_name = partner_name
+          order_line_item.dance_orientation = dance_orientation
+
+          ap order_line_item
+          order_line_item.save_without_timestamping
+        end
+      end
     end
   end
 
