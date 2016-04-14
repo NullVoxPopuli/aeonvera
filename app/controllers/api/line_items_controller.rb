37e765792731869ad0a6aad316dd7038bdbe1377
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-04-14 14:36:21 -0400

    Fixes #548

--- app/controllers/api/line_items_controller.rb
+++ app/controllers/api/line_items_controller.rb
@@ -2,10 +2,14 @@ class Api::LineItemsController < Api::EventResourceController
   private
 
   def update_line_item_params
-    params
-      .require(:data)
-      .require(:attributes)
-      .permit(:name, :price,)  end
+    ActiveModelSerializers::Deserialization
+      .jsonapi_parse(params, only: [
+        :name, :price, :description,
+        :expires_at, :starts_at, :ends_at, :becomes_available_at,
+        :duration_amount, :duration_unit,
+        :registration_opens_at, :registration_closes_at
+      ])
+  end
 
   def create_line_item_params
     create_params_with(update_line_item_params)
