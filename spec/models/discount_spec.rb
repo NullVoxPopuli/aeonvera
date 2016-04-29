c1ae010cfddca6608924de8aa62a45116cc76414
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-04-29 07:38:08 -0400

    fix discount spec

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -2,20 +2,21 @@ require 'spec_helper'
 
 describe Discount do
 
-	context '#code' do
-		it 'does not allow & for the code' do
-			d = Discount.new(code: "J&J", amount: 5)
-			expect(d.valid?).to eq false
-			msgs = d.errors.full_messages
-			expect(msgs.first).to include("Code")
-		end
-	end
+  context '#code' do
+    it 'does not allow & for the code' do
+      d = Discount.new(code: "J&J", amount: 5)
+      expect(d.valid?).to eq false
+      msgs = d.errors.full_messages
+      expect(msgs.first).to include("Code")
+    end
+  end
 
   context '#times_used' do
 
     it 'calculates based on orders' do
-      d = create(:discount, event: create(:event))
-			o = create(:order, event: d.event, attendance: create(:attendance, event: d.event))
+      e = create(:event)
+      d = create(:discount, host: e)
+      o = create(:order, host: e, attendance: create(:attendance, host: e))
       create(:order_line_item, line_item: d, order: o)
 
       expect(d.times_used).to eq 1
