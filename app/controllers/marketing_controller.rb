53bb85e92a6d73b03f4d175a985afdf73ff48468
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-05-02 07:54:13 -0400

    redirect away from subdomains

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -1,4 +1,6 @@
 class MarketingController < ActionController::Base
+  before_action :redirect_if_subdomain
+
   def index
     # get ember from redis
     ember_index = APICache.store.get('aeonvera:index:current-content')
@@ -7,4 +9,18 @@ class MarketingController < ActionController::Base
 
     render inline: ember_index
   end
+
+  private
+
+  def redirect_if_subdomain
+    domain = APPLICATION_CONFIG['domain'][Rails.env]
+    url = request.url
+    protocol, url = request.url.split('://')
+    subdomain = url.sub('.' + domain, '').sub('/', '')
+
+    if subdomain != 'www'
+      redirect_to "#{protocol}://#{domain}/#{subdomain}/"
+    end
+  end
+
 end
---
+++ spec/requests/application_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'Subdomains not allowed', type: :request do
+
+  context 'GET subdomain.whatever' do
+    let(:domain) { APPLICATION_CONFIG['domain'][Rails.env] }
+
+    it 'redirects' do
+      get "https://subdomain1.#{domain}"
+      expect(response.status).to eq 302
+      expect(response.location).to eq "https://#{domain}/subdomain1/"
+    end
+
+    it 'respects the protocol' do
+      get "http://subdomain2.#{domain}"
+      expect(response.status).to eq 302
+      expect(response.location).to eq "http://#{domain}/subdomain2/"
+    end
+
+    it 'removes the path' do
+      pending('currently, ember handles this, but how do we do it in rails?')
+      get "http://subdomain3.#{domain}/this/does/not/matter"
+      expect(response.status).to eq 302
+      expect(response.location).to eq "http://#{domain}/subdomain3/"
+    end
+
+    it 'does not redirect www' do
+      get "http://www.#{domain}/"
+      expect(response.status).to_not eq 302
+    end
+  end
+
+end
