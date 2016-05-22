233b148f92c80cda12df3c0ac2f2dc9740a93397
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-05-22 18:10:23 -0400

    clean up the pricing tiers controller

--- app/controllers/api/pricing_tiers_controller.rb
+++ app/controllers/api/pricing_tiers_controller.rb
@@ -1,35 +1,21 @@
 class Api::PricingTiersController < Api::EventResourceController
   private
 
-  def deserialized_params
-    ActiveModelSerializers::Deserialization
-      .jsonapi_parse(params)
-  end
-
   # The fields are named different on teh server than the client
   def update_pricing_tier_params
-    whitelister = ActionController::Parameters.new(deserialized_params)
-    rp = whitelister.permit(
-      :increase_by_dollars,
-      :date, :increase_after_date,
-      :registrants, :increase_by)
-
-    # This is gross
-    rp[:increase_by_dollars] = rp[:increase_by] unless rp[:increase_by_dollars]
-    rp[:date] = rp[:increase_after_date] unless rp[:date]
-    rp[:registrants] = rp[:increase_after_total_registrants] unless rp[:date]
-    # This is gross
-    rp.slice(*PricingTier.columns.map(&:name).map(&:to_sym))
-    # Maybe think about naming the fields in ember-land the same as in rails
+    whitelistable_params do |whitelister|
+      whitelister.permit(
+        :date,
+        :registrants, :increase_by_dollars)
+    end
   end
 
   def create_pricing_tier_params
-    attributes = update_pricing_tier_params
-
-    event_relationship = params
-      .require(:data).require(:relationships)
-      .require(:event).require(:data).permit(:id)
-
-    attributes.merge(event_id: event_relationship[:id])
+    whitelistable_params do |whitelister|
+      whitelister.permit(
+        :event_id,
+        :date,
+        :registrants, :increase_by_dollars)
+    end
   end
 end
