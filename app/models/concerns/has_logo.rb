cca758db8bc1b5cae498afd3903d79fbf16bd610
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-05-25 21:14:51 -0400

    fixed the upload

--- app/models/concerns/has_logo.rb
+++ app/models/concerns/has_logo.rb
@@ -9,7 +9,10 @@ module HasLogo
         medium: '300x300>'
       }
 
-    validates_attachment_file_name :logo, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+    validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
+    # do_not_validate_attachment_file_type :logo
+    # validates_attachment_file_name :logo, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+
   end
 
 end
