6a2166a9081682458c5f8ebcba30db4e0332e4de
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-06-25 16:34:55 -0400

    add relationships

--- app/serializers/order_line_item_serializer.rb
+++ app/serializers/order_line_item_serializer.rb
@@ -8,7 +8,19 @@ class OrderLineItemSerializer < ActiveModel::Serializer
       k.gsub('line-item/', '') # cause ember doesn't have nesting
     }
 
-    attributes :id, :name
+    attributes :id, :name, :amount
+
+    has_many :restraints
+    def restraints
+      # not all line items have restraints
+      # though, they probably should
+      object.try(:restraints) || []
+    end
+
+    def amount
+      # hack for discounts :-(
+      object.try(:amount)
+    end
   end
 
   belongs_to :line_item, serializer: OrderLineItemSerializer::LineItemSerializer
