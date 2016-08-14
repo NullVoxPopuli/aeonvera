13b8a9288af0842066a38d8c1c84bf6936a50ae9
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-08-13 21:20:45 -0400

    now, everything in ransackable by default

--- config/initializers/skinny_controllers.rb
+++ config/initializers/skinny_controllers.rb
@@ -1,1 +1,18 @@
+# frozen_string_literal: true
 SkinnyControllers.controller_namespace = 'Api'
+
+module SkinnyControllers
+  module Operation
+    module ModelHelpers
+      def model_from_named_id(key, id)
+        name = key.gsub(/_id$/, '')
+        name = name.camelize
+        model_from_scope(
+          id: id,
+          type: name
+        ).ransack(params[:q]).result
+        # only this last line was added for searching with ransack
+      end
+    end
+  end
+end
