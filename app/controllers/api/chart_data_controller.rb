b85e542ff31413e457a67a98ff8c3430f7b67df4
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-08-14 22:03:14 -0400

    new chart

--- app/controllers/api/chart_data_controller.rb
+++ app/controllers/api/chart_data_controller.rb
@@ -3,7 +3,18 @@ module Api
     include SetsEvent
 
     def show
-      render json: @event, serializer: IncomeAndRegistrationsChartSerializer
+      render json: @event, serializer: chart_serializer
+    end
+
+    private
+
+    def chart_serializer
+      chart_type = params[:chart_type]
+
+      case chart_type
+      when 'line-income-and-registrations' then Chart::IncomeAndRegistrationsChartSerializer
+      when 'sunburt-registration-breakdown' then Chart::RegistrationBreakdownSerializer
+      end
     end
   end
 end
---
+++ app/serializers/chart/income_and_registrations_chart_serializer.rb
@@ -0,0 +1,82 @@
+module Chart
+  class IncomeAndRegistrationsChartSerializer < ActiveModel::Serializer
+    type 'charts'
+
+    attributes :id,
+      :incomes, :registrations,
+      :income_times,
+      :registration_times
+
+    def id
+      'income-and-registrations'
+    end
+
+    def incomes
+      data[:incomes]
+    end
+
+    def registrations
+      data[:registrations]
+    end
+
+    def income_times
+      data[:income_times]
+    end
+
+    def registration_times
+      data[:registration_times]
+    end
+
+
+
+    private
+
+    def data
+      unless @data
+        registration_times = []
+        registrations = []
+        income_times = []
+        income = []
+
+        count = 0
+        money = 0
+
+        attendances = object.attendances.reorder("created_at ASC")
+        attendances.each do |attendance|
+          time = attendance.created_at.to_i
+          registration_times << time.to_s
+          registrations << (count += 1).to_s
+        end
+
+        orders = object.orders.reorder("created_at ASC")
+        orders.each do |order|
+          time = order.created_at.to_i
+          income_times << time.to_s
+          income << (money += (order.net_amount_received || 0)).round(2)
+        end
+
+        if (i_last = income_times.last) != (r_last = registration_times.last)
+          if i_last > r_last
+            registration_times << i_last
+            registrations << registrations.last
+          else
+            income_times << r_last
+            income << income.last
+          end
+        end
+
+        @data = {
+          incomes: income,
+          registrations: registrations,
+          income_times: income_times,
+          registration_times: registration_times
+        }
+      end
+
+      @data
+    end
+
+
+  end
+
+end
