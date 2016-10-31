b30ccb76d8241b4da742e6bf3115e9b5f1708e0a
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-10-31 07:34:48 -0400

    to RFC822

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -2,6 +2,8 @@ require 'rspec/expectations'
 
 RSpec::Matchers.define :be_the_same_time_as do |expected|
   match do |actual|
-    actual.utc.to_i == expected.utc.to_i
+    actual.utc.to_formatted_s(:rfc822) == expected.utc.to_formatted_s(:rfc822)
   end
+
+  diffable
 end
