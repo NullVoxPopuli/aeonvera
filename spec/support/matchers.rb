8ce86347353adc6bd1bd992140d5849b2286f2de
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-10-31 07:43:17 -0400

    debug output

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -2,6 +2,8 @@ require 'rspec/expectations'
 
 RSpec::Matchers.define :be_the_same_time_as do |expected|
   match do |actual|
+    ap actual.utc.to_formatted_s(:rfc822)
+    ap expected.utc.to_formatted_s(:rfc822)
     actual.utc.to_formatted_s(:rfc822) == expected.utc.to_formatted_s(:rfc822)
   end
 
