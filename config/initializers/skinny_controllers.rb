b0256408000a3ffbc7d82e61e32fc5a7b978488b
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-11-30 21:22:57 -0500

    upgrade skinny_controllers

--- config/initializers/skinny_controllers.rb
+++ config/initializers/skinny_controllers.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
-SkinnyControllers.controller_namespace = 'Api'
-
-module SkinnyControllers
-  module Operation
-    module ModelHelpers
-      def model_from_named_id(key, id)
-        name = key.gsub(/_id$/, '')
-        name = name.camelize
-        model_from_scope(
-          id: id,
-          type: name
-        ).ransack(params[:q]).result
-        # only this last line was added for searching with ransack
-      end
-    end
-  end
+SkinnyControllers.search_proc = lambda do |relation|
+  relation.ransack(params[:q]).result
 end
+# module SkinnyControllers
+#   module Operation
+#     module ModelHelpers
+#       def model_from_named_id(key, id)
+#         name = key.gsub(/_id$/, '')
+#         name = name.camelize
+#         model_from_scope(
+#           id: id,
+#           type: name
+#         ).ransack(params[:q]).result
+#         # only this last line was added for searching with ransack
+#       end
+#     end
+#   end
+# end
