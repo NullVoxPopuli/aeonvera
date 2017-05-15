4c36c0cb076da1c79194aa19037175509460c799
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-05-14 20:50:06 -0400

    progress

--- app/resources/api/registrations/controller.rb
+++ app/resources/api/registrations/controller.rb
@@ -1,15 +1,19 @@
+# frozen_string_literal: true
+
 module Api
   class RegistrationsController < APIController
-
     def index
-      @attendances = current_user.attendances
-      render json: @attendances, each_serializer: RegistrationSummarySerializer
+      search = current_user.attendances.ransack(params[:q])
+      @attendances = search.result
+
+      render json: @attendances,
+             include: params[:include],
+             each_serializer: RegistrationSummarySerializer
     end
 
     def show
       @attendance = current_user.attendances.find(params[:id])
       render json: @attendance
     end
-
   end
 end
