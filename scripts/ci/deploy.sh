27945976dbd521a6eed7419c692fc7837b3824a0
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-06-25 11:16:06 -0400

    bah

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -4,5 +4,5 @@ curl -n -X POST \
   "https://api.heroku.com/apps/$HEROKU_APP_NAME/ps" \
   -H "Accept: application/json" \
   -H "Authorization: Bearer ${HEROKU_API_KEY}" \
-  -d "command=bundle exec rails db:migrate"
+  -d "command=bundle exec rake db:migrate"
 
