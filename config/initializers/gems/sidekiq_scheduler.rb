60a27325339488b0459428de9339c4c01eaadc76
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-07-05 19:55:26 -0400

    add sidekiq-web to production

--- config/initializers/gems/sidekiq_scheduler.rb
+++ config/initializers/gems/sidekiq_scheduler.rb
@@ -40,8 +40,19 @@ else
   puts "Sidekiq::Scheduler.enabled is #{Sidekiq::Scheduler.enabled.inspect}"
 end
 
-Sidekiq::Web.use(Rack::Auth::Basic) do |email, password|
-  # user = User.find_by_email(email)
-  # user.try(:authenticate, password)
-  true
+Sidekiq::Web.use(Rack::Auth::Basic) do |username, password|
+  # Protect against timing attacks:
+  # - See https://codahale.com/a-lesson-in-timing-attacks/
+  # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+  # - Use & (do not use &&) so that it doesn't short circuit.
+  # - Use digests to stop length information leaking
+  #  (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+  ActiveSupport::SecurityUtils.secure_compare(
+    ::Digest::SHA256.hexdigest(username),
+    ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])
+  ) &
+    ActiveSupport::SecurityUtils.secure_compare(
+      ::Digest::SHA256.hexdigest(password),
+      ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD'])
+    )
 end
