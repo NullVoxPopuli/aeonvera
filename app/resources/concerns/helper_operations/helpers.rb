22ac2bc9ed788564fec681b6233614d8cf783f02
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-07-11 23:45:33 -0400

    type check

--- app/resources/concerns/helper_operations/helpers.rb
+++ app/resources/concerns/helper_operations/helpers.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
 module HelperOperations
   module Helpers
     def host_from_params(params)
       id, host_type = params.values_at(:host_id, :host_type)
 
-      if host_type.downcase.include?('organization')
+      if host_type&.downcase&.include?('organization')
         Organization.find(id)
       else
         Event.find(id)
