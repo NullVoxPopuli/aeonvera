32648b48a2b78d4b964f643b4c06bab67a78968e
Preston Sego <LPSego3+dev@gmail.com>
2017-07-14 08:24:56 -0400

    blegh

--- app/resources/api/collaborations/controller.rb
+++ app/resources/api/collaborations/controller.rb
@@ -1,8 +1,19 @@
 # this is where collaborations are managed
 # collaborations are accepted at users/collaborations
 module Api
-  class CollaborationsController < Api::ResourceController
+  class CollaborationsController < UserResourceController
+    include ::HelperOperations::Helpers
+
+    self.resource_class = ::Collaboration
+    self.serializer_class = CollaborationSerializer
+    self.parent_resource_method = :host
+    self.association_name_for_parent_resource = :collaborations
+
     before_filter :must_be_logged_in
+
+    def host
+      host_from_params(params)
+    end
 
     private
 
