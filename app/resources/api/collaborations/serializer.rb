8d49f81978a76ba21dbda26bcc79295033409a0a
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-07-29 07:26:24 -0400

    eh?

--- app/resources/api/collaborations/serializer.rb
+++ app/resources/api/collaborations/serializer.rb
@@ -1,9 +1,14 @@
+# frozen_string_literal: true
 module Api
   class CollaborationSerializer < ActiveModel::Serializer
     attributes :id, :email,
       :user_name, :title, :created_at
 
     belongs_to :host
+
+    def id
+      object.id || 'invited'
+    end
 
     def host
       object.collaborated
