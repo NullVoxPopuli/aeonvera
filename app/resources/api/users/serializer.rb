b0db7e0774713f1d67e89a45750d450846f5c189
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-07-31 06:38:30 -0400

    formatting

--- app/resources/api/users/serializer.rb
+++ app/resources/api/users/serializer.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 module Api
   class UserSerializer < ActiveModel::Serializer
-
     attributes :id,
-      :first_name, :last_name,
-      :email,
-      :sign_in_count,
-      :current_sign_in_at, :current_sign_in_ip,
-      :confirmed_at, :confirmation_sent_at,
-      :unconfirmed_email,
-      :time_zone
+               :first_name, :last_name,
+               :email,
+               :sign_in_count,
+               :current_sign_in_at, :current_sign_in_ip,
+               :confirmed_at, :confirmation_sent_at,
+               :unconfirmed_email,
+               :time_zone
 
     has_many :membership_renewals, each_serializer: MembershipRenewalSerializer
 
