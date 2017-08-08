2c7757b5ba4dfa1dde8c4b71bfb462cdc02dd45b
Preston Sego <LPSego3+dev@gmail.com>
2017-08-08 17:56:07 -0400

    woo

--- app/resources/api/application_presenter.rb
+++ app/resources/api/application_presenter.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 module Api
-  class ApplicationPresenter
+  class ApplicationPresenter < SimpleDelegator
     attr_reader :object
 
     def initialize(object)
       @object = object
+
+      super(object)
     end
   end
 end
