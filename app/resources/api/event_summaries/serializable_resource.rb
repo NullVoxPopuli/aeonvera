8e5795f82e98c9fa1aaacbafc4c07867df528587
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-08-19 09:45:11 -0400

    support new graph

--- app/resources/api/event_summaries/serializable_resource.rb
+++ app/resources/api/event_summaries/serializable_resource.rb
@@ -10,7 +10,10 @@ module Api
                :registration_opens_at, :starts_at, :ends_at, :url,
                :revenue, :unpaid
 
-    attribute(:revenue) { (@object.revenue || 0).to_f }
+    attribute(:revenue) { @object.orders.paid.sum(:current_net_amount_received) }
+    attribute(:revenue_gross) { @object.orders.paid.sum(:current_paid_amount) }
+    attribute(:revenue_fees) { @object.orders.paid.sum(:current_total_fee_amount) }
+
     attribute(:unpaid) { (@object.unpaid_total || 0).to_f }
     attribute(:number_of_shirts_sold) { @object.shirts_sold }
     attribute(:my_event) { @object.hosted_by_id == @current_user&.id }
