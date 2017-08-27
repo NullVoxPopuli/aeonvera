56172d6e945f1b36fd31059eed8e364b94837c91
Preston Sego <LPSego3+dev@gmail.com>
2017-08-27 09:23:42 -0400

    add deleted_at to registration on events

--- app/resources/api/events/registrations/serializable_resource.rb
+++ app/resources/api/events/registrations/serializable_resource.rb
@@ -4,6 +4,8 @@ module Api
   module Events
     class RegistrationSerializableResource < Users::RegistrationSerializableResource
       type 'events/registrations'
+
+      attribute :deleted_at
     end
   end
 end
