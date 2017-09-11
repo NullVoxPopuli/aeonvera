1da081cfae25c7af79548edb7c879930c71ee160
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-09-11 07:39:18 -0400

    Blegh

--- app/services/csv_generation.rb
+++ app/services/csv_generation.rb
@@ -6,7 +6,7 @@ module CsvGeneration
   def model_to_csv(model, fields_from_params = nil, skip_serialization: false, serializer:)
     options = { class: serializer }
 
-    options.merge(options_from_fields(fields_from_params))
+    options.merge(options_from_fields(fields_from_params, serializer))
 
     hash = skip_serialization ? model : serialize_data(options, model)
 
@@ -14,31 +14,65 @@ module CsvGeneration
   end
 
   def serialize_data(options, model)
-    jsonapi = renderer.render(model, options)
+    jsonapi = renderer.render(Array[*model], options)
+    data = jsonapi[:data]
 
-    included = {}
-    jsonapi[:included].each do |i|
-     included[i[:type]] ||= {}
-     included[i[:id]] = i[:attributes]
-    end if jsonapi[:included]
+    included = normalize_included(jsonapi[:included])
 
-    hash = jsonapi[:data].map do |d|
-     r = d[:attributes]
-     d[:relationships].each do |k, v|
-       id = v.dig(:data, :id)
-       next unless id
-       relations = included.dig(k, id)
-       next unless relations
-       relations.each do |j, x|
-         r[j] = x
-       end
-     end
-    end
+    hash = data.map do |d|
+      merge_attributes_with_data(d[:attributes], d[:relationships], included, options)
+    end.compact
 
     flat_hash(hash)
   end
 
-  def options_from_fields(fields_from_params)
+  # TODO: make idiomatic
+  def normalize_included(included)
+    return {} unless included
+    result = {}
+
+    included.each do |i|
+      result[i[:type]] ||= {}
+      result[i[:type]][i[:id]] = i[:attributes]
+    end
+
+    result
+  end
+
+  def merge_attributes_with_data(attributes, relationships, included, options)
+    attributes ||= {}
+    included ||= {}
+    result = attributes
+    serializer = options[:class]
+    fields = options[:fields]
+    relationships = Array[*relationships]
+
+    relationships.each do |k, v|
+      next unless fields
+      id = v.dig(:data, :id)
+      next unless id
+
+      field_relation_key = k.to_s.pluralize.to_sym
+
+      field_relation = fields[field_relation_key]
+      next unless field_relation.present?
+
+      relation = serializer.relationship_options[k]
+      # No support for polymorphic types
+      next unless relation.is_a?(String)
+      key = relation[:class].constantize.type_val
+      relations = included.dig(key, id)
+      next unless relations
+
+      relations.each do |j, x|
+        result[j] = x if field_relation.include?(j)
+      end
+    end
+
+    result
+  end
+
+  def options_from_fields(fields_from_params, serializer)
     return {} unless fields_from_params
 
     fields = JSONAPI::IncludeDirective.new(fields_from_params).to_hash
@@ -47,17 +81,23 @@ module CsvGeneration
     fields.each do |k, v|
       if !v.values.empty?
         key = k.to_s.pluralize.underscore.to_sym
-        f[k.to_s.pluralize.to_sym] = v.keys.map(&:to_s).map(&:underscore)
-        i << key
+        f[k.to_s.pluralize.to_sym] = v.keys.map(&:to_s).map(&:underscore).map(&:to_sym)
+        i << k
       else
         f[k] = v
       end
     end
 
-    {
-      fields: CaseTransform.underscore(f),
+    type = serializer.type_val
+
+    underscored = CaseTransform.underscore(f)
+    options = {
+      fields: underscored
+              .merge(type => underscored.keys.map(&:to_s).map(&:singularize).map(&:to_sym)),
       include: i.join(',')
     }
+
+    options
   end
 
   def renderer
@@ -65,7 +105,9 @@ module CsvGeneration
   end
 
   def flat_hash(hash)
+    return [] if hash.blank?
     return hash.map { |h| flat_hash(h) } if hash.is_a?(Array)
+
     result = {}
     hash.each do |k, v|
       if v.is_a?(Hash)
--- spec/services/csv_generation_spec.rb
+++ spec/services/csv_generation_spec.rb
@@ -1,23 +1,42 @@
 # frozen_string_literal: true
 
 describe CsvGeneration do
+  let(:housing_request) { create(:housing_request, registration: create(:registration)) }
+  let(:resource_class) { ::Api::HousingRequestSerializableResource }
+
+  before(:each) { pending('CSVs are hard'); fail }
+
   describe '.serialize_data' do
-    let(:housing_request) { create(:housing_request) }
+    let(:options) do
+      { class: resource_class }
+    end
 
     subject { ->(*args) { described_class.serialize_data(*args) } }
-    before(:each) { pending('csvs are hard') }
+
     it 'renders only the default fields' do
-      result = subject.call([housing_request], nil, serializer: ::Api::HousingRequestSerializableResource)
+      result = subject.call(options, [housing_request])
 
       expect(result).to eq [
         {
-
+          notes: housing_request.notes,
+          name: housing_request.name,
+          need_transportation: housing_request.need_transportation,
+          can_provide_transportation: housing_request.can_provide_transportation,
+          transportation_capacity: housing_request.transportation_capacity,
+          allergic_to_pets: housing_request.allergic_to_pets,
+          allergic_to_smoke: housing_request.allergic_to_smoke,
+          other_allergies: housing_request.other_allergies,
+          requested_roommates: housing_request.requested_roommates,
+          unwanted_roommates: housing_request.unwanted_roommates,
+          preferred_gender_to_house_with: housing_request.preferred_gender_to_house_with,
+          housing_provision_id: housing_request.housing_provision_id
         }
       ]
     end
 
     it 'renders specific fields' do
-      result = subject.call([housing_request], [:transportation_capacity], serializer: ::Api::HousingRequestSerializableResource)
+      options[:fields] = { 'housing-requests': [:transportation_capacity] }
+      result = subject.call(options, [housing_request])
 
       expect(result).to eq [
         {
@@ -27,13 +46,86 @@ describe CsvGeneration do
     end
 
     it 'renders fields on a relationship' do
-      result = subject.call([housing_request], [registrations: [:attendee_name]], serializer: ::Api::HousingRequestSerializableResource)
+      options = described_class.options_from_fields('registration.attendee_name', resource_class).merge(class: resource_class)
+      result = subject.call(options, [housing_request])
 
       expect(result).to eq [
         {
           attendee_name: housing_request.registration.attendee_name
         }
       ]
+    end
+
+    it 'includes a mix of fields and relational fields' do
+      options = described_class.options_from_fields('transportation_capacity,registration.attendee_name', resource_class).merge(class: resource_class)
+      result = subject.call(options, [housing_request])
+
+      expect(result).to eq [
+        {
+          attendee_name: housing_request.registration.attendee_name,
+          transportation_capacity: housing_request.transportation_capacity
+        }
+      ]
+    end
+  end
+
+  describe '.options_from_fields' do
+    subject { ->(*args) { described_class.options_from_fields(*args) } }
+
+    it 'sets included' do
+      result = subject.call('relationship.attendee_name,other.email', resource_class)
+
+      expect(result).to eq({
+                             fields: {
+                               'housing-requests': [:relationship, :other],
+                               relationships: [:attendee_name],
+                               others: [:email] },
+                             include: 'relationship,other'
+                           })
+    end
+  end
+
+  describe '.merge_attributes_with_data' do
+    subject { ->(*args) { described_class.merge_attributes_with_data(*args) } }
+
+    it 'has no attributes, a relationship, and nothing included' do
+      result = subject.call(
+        nil, {
+          registration: {
+            data: {
+              type: :'users/registrations',
+              id: housing_request.registration.id
+            }
+          }
+        },
+        nil,
+        { class: resource_class }
+      )
+
+      expect(result).to eq({})
+    end
+
+    it 'has no attributes, a relationship, and included' do
+      result = subject.call(
+        nil, {
+          registration: {
+            data: {
+              type: :'users/registrations',
+              id: housing_request.registration.id
+            }
+          }
+        },
+        {
+          'users/registrations': {
+            housing_request.registration.id => {
+              attendee_name: 'name'
+            }
+          }
+        },
+        { class: resource_class, fields: { registrations: [:attendee_name] } }
+      )
+
+      expect(result).to eq({ attendee_name: 'name' })
     end
   end
 end
